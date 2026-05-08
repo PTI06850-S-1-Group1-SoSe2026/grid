b19016abe1c5cfee30f4010afee7563b06dc6915
Marvin Möckel <mail@marvinmoeckel.me>
2026-05-08 11:24:53 +0200

    enabled tracking of multiple markers

--- Assets/Scripts/TrackablesManager.cs
+++ Assets/Scripts/TrackablesManager.cs
@@ -1,14 +1,22 @@
 using Meta.XR.MRUtilityKit;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TrackablesManager : MonoBehaviour
 {
     [SerializeField] private GameObject trackedObjectPrefab;
+    [SerializeField] private Dictionary<string, GameObject> trackedObjects = new();
 
     public void OnTrackableAdded(MRUKTrackable trackable)
     {
         Debug.Log($"Trackable of type {trackable.TrackableType} added");
-        Instantiate(trackedObjectPrefab, trackable.transform);
+
+        if (trackable.TrackableType == OVRAnchor.TrackableType.QRCode && trackable.MarkerPayloadString != null)
+        {
+            Debug.LogError($"Detected QR code: {trackable.MarkerPayloadString}");
+            GameObject markerIndicator = Instantiate(trackedObjectPrefab, trackable.transform);
+            trackedObjects.Add(trackable.MarkerPayloadString, markerIndicator);
+        }
     }
 
     public void OnTrackableRemoved(MRUKTrackable trackable)
