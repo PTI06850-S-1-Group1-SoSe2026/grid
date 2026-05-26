e463baaef1cb2b3c54a2e60264766ac4aa7ec4c6
Marvin Möckel <mail@marvinmoeckel.me>
2026-05-26 14:33:00 +0200

    removed import and reformated CubeShape.cs

--- Assets/GridShapes/CubeShape.cs
+++ Assets/GridShapes/CubeShape.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CubeShape : GridShape
 {
     private readonly GameObject _baseShape = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-    public override GameObject BaseShapePrefab { get { return _baseShape; } }
+    public override GameObject BaseShapePrefab
+    {
+        get { return _baseShape; }
+    }
 
     public override List<Vector3> GetPoints(float density)
     {
@@ -28,16 +30,26 @@ public class CubeShape : GridShape
                     var pointSet = new List<Vector3>()
                     {
                         (i < bounds.y && f < bounds.x)
-                          ? new Vector3(i - (bounds.x / 2) + (bounds.x / density / 2), f - (bounds.y / 2) + (bounds.x / density / 2), side)
-                          : new Vector3(),
-                        new(i - (bounds.x / 2), f - (bounds.y / 2), side)
+                            ? new Vector3(
+                                i - (bounds.x / 2) + (bounds.x / density / 2),
+                                f - (bounds.y / 2) + (bounds.x / density / 2),
+                                side
+                            )
+                            : new Vector3(),
+                        new(i - (bounds.x / 2), f - (bounds.y / 2), side),
                     };
                     points.AddRange(pointSet);
 
                     // add other sides
                     foreach (Vector3 point in pointSet)
                     {
-                        points.AddRange(new Vector3[] { Quaternion.AngleAxis(90f, Vector3.up) * (point - pivot) + pivot, Quaternion.AngleAxis(90f, Vector3.right) * (point - pivot) + pivot });
+                        points.AddRange(
+                            new Vector3[]
+                            {
+                                Quaternion.AngleAxis(90f, Vector3.up) * (point - pivot) + pivot,
+                                Quaternion.AngleAxis(90f, Vector3.right) * (point - pivot) + pivot,
+                            }
+                        );
                     }
                 }
             }
@@ -46,7 +58,15 @@ public class CubeShape : GridShape
         return points;
     }
 
-    public List<Vector3> GetPoints(float density, bool top, bool bottom, bool left, bool right, bool front, bool back)
+    public List<Vector3> GetPoints(
+        float density,
+        bool top,
+        bool bottom,
+        bool left,
+        bool right,
+        bool front,
+        bool back
+    )
     {
         var points = this.GetPoints(density);
 
