c3b83cbf1c1cad6a3296399e1d1bcacdd8b74b37
Marvin Möckel <mail@marvinmoeckel.me>
2026-05-26 14:01:32 +0200

    Feature/place anchor by click (#28)

--- Assets/Scripts/TrackablesManager.cs
+++ Assets/Scripts/TrackablesManager.cs
@@ -2,16 +2,40 @@ using System.Collections.Generic;
 using Meta.XR.MRUtilityKit;
 using UnityEngine;
 
+/**
+ * Fixing the indicators works as follows:
+ * If QR1 has been recognized already and the controller trigger gets triggered,
+ * its indicator gets fixed within the world. After that, the same thing happens
+ * to the indicator of QR2, if the button gets triggered again.
+ */
 public class TrackablesManager : MonoBehaviour
 {
+    public enum PlacementState
+    {
+        None,
+        QR1Fixed,
+        QR2Fixed,
+    }
+
     [SerializeField]
     private GameObject trackedObjectPrefab;
 
     private Dictionary<string, GameObject> trackedObjects = new();
 
+    public static string QR1_NAME = "QR_1";
+    public static string QR2_NAME = "QR_2";
+
+    private PlacementState currentState = PlacementState.None;
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+            HandlePlacement();
+    }
+
     public void OnTrackableAdded(MRUKTrackable trackable)
     {
-        Debug.Log($"Trackable of type {trackable.TrackableType} added");
+        Debug.LogError($"Trackable of type {trackable.TrackableType} added");
 
         if (
             trackable.TrackableType == OVRAnchor.TrackableType.QRCode
@@ -26,7 +50,56 @@ public class TrackablesManager : MonoBehaviour
 
     public void OnTrackableRemoved(MRUKTrackable trackable)
     {
-        Debug.Log($"Trackable of type {trackable.TrackableType} removed");
-        Destroy(transform.gameObject);
+        Debug.LogError($"Trackable of type {trackable.TrackableType} removed");
+
+        if (trackable.TrackableType == OVRAnchor.TrackableType.QRCode)
+        {
+            if (trackedObjects.TryGetValue(trackable.MarkerPayloadString, out GameObject marker))
+            {
+                Destroy(marker);
+                trackedObjects.Remove(trackable.MarkerPayloadString);
+            }
+        }
+    }
+
+    private void HandlePlacement()
+    {
+        switch (currentState)
+        {
+            case PlacementState.None:
+                if (FixIndicator(QR1_NAME))
+                {
+                    currentState = PlacementState.QR1Fixed;
+                    Debug.LogError($"QR Code '{QR1_NAME}' has been fixed");
+                }
+                break;
+
+            case PlacementState.QR1Fixed:
+                if (FixIndicator(QR2_NAME))
+                {
+                    currentState = PlacementState.QR2Fixed;
+                    Debug.LogError($"QR code '{QR2_NAME}' has been fixed");
+                }
+                break;
+
+            case PlacementState.QR2Fixed:
+                Debug.LogError("All Indicators have already been fixed");
+                break;
+        }
+    }
+
+    private bool FixIndicator(string qrName)
+    {
+        if (trackedObjects.TryGetValue(qrName, out GameObject obj))
+        {
+            // detach indicator from marker (qr code)
+            obj.transform.SetParent(null);
+
+            trackedObjects.Remove(qrName);
+            return true;
+        }
+
+        Debug.LogError($"QR code {qrName} wasn't recognized yet");
+        return false;
     }
 }
